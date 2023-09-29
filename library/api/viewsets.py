8dfb7f1645056242e0d0e5251bc5624897a50d8c
jon_prog <jonashenri.aquino@gmail.com>
2023-09-29 05:09:36 -0300

    create search category

--- library/api/viewsets.py
+++ library/api/viewsets.py
@@ -6,9 +6,9 @@ class BooksViewset(viewsets.ModelViewSet):
     serializer_class = serializers.BookSerializer
     
     def get_queryset(self):
-        category_slug = self.request.query_params.get('category')
-        if category_slug:
-            return models.Book.objects.filter(category_slug=category_slug)
+        category = self.request.query_params.get('category')
+        if category:
+            return models.Book.objects.filter(category__slug=category)
         else:
             return models.Book.objects.all()
 
