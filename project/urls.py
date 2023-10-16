77dc7321c182f64e0545d33e8c60830caf8654cf
jon_prog <jonashenri.aquino@gmail.com>
2023-10-15 22:06:08 -0300

    add swagger and redoc in project

--- project/urls.py
+++ project/urls.py
@@ -20,14 +20,36 @@ from django.urls import path, include
 from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from library.api import viewsets
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 route = routers.DefaultRouter() 
 
 route.register(r'books', viewsets.BooksViewset, basename='Books')
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('', include(route.urls)),
     path('token/',TokenObtainPairView.as_view()),
     path('token/refresh/',TokenRefreshView.as_view()),
     path('admin/', admin.site.urls),
+]
+
+# swagger
+urlpatterns += [
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),  # noqa E501
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
