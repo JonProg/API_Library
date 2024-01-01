c412607809c42f49ebe6089734c43a915f4f19cd
jon_prog <jonashenri.aquino@gmail.com>
2024-01-01 14:40:27 -0300

    update:settings

--- project/settings.py
+++ project/settings.py
@@ -123,7 +123,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 }
 
 
